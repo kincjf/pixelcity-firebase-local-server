86d50ccfa4f6253bc60c8df2f8386d93d461cdc1
KIMSEONHO <sinho0689@gmail.com>
2017-11-10 00:32:26 +0900

    - character.js 수정(html docs 참고)
    Signed-off-by: KIMSEONHO <sinho0689@gmail.com>

--- test/data/character.js
+++ test/data/character.js
@@ -1,22 +1,22 @@
 "use strict";
 
 var data = {
-	"Pyok": {
-		"elementType": "tree",
-		"dropItems": {
-			"Sword": true,
-			"Redberry": true
-		},
-		"dropGameMoney": {
-			"min": 50,
-			"max": 60
-		},
-		"spawnRate": 20,
-		"rating": 2,
-		"monster": true,
-		"boss": false,
-		"properties": {
-			"lv1": {
+	"field": {
+		"Pyok": {
+			"elementType": "tree",
+			"dropItems": {
+				"Sword": true,
+				"Redberry": true
+			},
+			"dropGameMoney": {
+				"min": 50,
+				"max": 60
+			},
+			"spawnRate": 20,
+			"rating": 2,
+			"monster": true,
+			"boss": false,
+			"properties": {
 				"hp": {
 					"min": 450,
 					"max": 500
@@ -33,29 +33,31 @@ var data = {
 					"min": 1.0,
 					"max": 0.5
 				},
-				"expForNext": 200,
-				"skills": {
-					"LeafBlast": true
+				"byLevel": {
+					"lv1": {
+						"expForNext": 200,
+						"skills": {
+							"LeafBlast": true
+						}
+					}
 				}
 			}
-		}
-	},
-	"Xyz": {
-		"elementType": "dark",
-		"dropItems": {
-			"GreenBow": true,
-			"SilverCoin": true
 		},
-		"dropGameMoney": {
-			"min": 50,
-			"max": 60
-		},
-		"spawnRate": 20,
-		"rating": 2,
-		"monster": true,
-		"boss": false,
-		"properties": {
-			"lv1": {
+		"Xyz": {
+			"elementType": "dark",
+			"dropItems": {
+				"GreenBow": true,
+				"SilverCoin": true
+			},
+			"dropGameMoney": {
+				"min": 50,
+				"max": 60
+			},
+			"spawnRate": 20,
+			"rating": 2,
+			"monster": true,
+			"boss": false,
+			"properties": {
 				"hp": {
 					"min": 450,
 					"max": 500
@@ -72,29 +74,31 @@ var data = {
 					"min": 2.0,
 					"max": 1.0
 				},
-				"expForNext": 200,
-				"skills": {
-					"MorningAmericano": true
+				"byLevel": {
+					"lv1": {
+						"expForNext": 200,
+						"skills": {
+							"MorningAmericano": true
+						}
+					}
 				}
 			}
-		}
-	},
-	"Inish": {
-		"elementType": "fire",
-		"dropItems": {
-			"BronzeCoin": true,
-			"Blade": true
 		},
-		"dropGameMoney": {
-			"min": 50,
-			"max": 60
-		},
-		"spawnRate": 20,
-		"rating": 2,
-		"monster": true,
-		"boss": false,
-		"properties": {
-			"lv1": {
+		"Inish": {
+			"elementType": "fire",
+			"dropItems": {
+				"BronzeCoin": true,
+				"Blade": true
+			},
+			"dropGameMoney": {
+				"min": 50,
+				"max": 60
+			},
+			"spawnRate": 20,
+			"rating": 2,
+			"monster": true,
+			"boss": false,
+			"properties": {
 				"hp": {
 					"min": 500,
 					"max": 600
@@ -111,29 +115,31 @@ var data = {
 					"min": 1.0,
 					"max": 0.5
 				},
-				"expForNext": 200,
-				"skills": {
-					"RussianRoulette": true
+				"byLevel": {
+					"lv1": {
+						"expForNext": 200,
+						"skills": {
+							"RussianRoulette": true
+						}
+					}
 				}
 			}
-		}
-	},
-	"Luga": {
-		"elementType": "water",
-		"dropItems": {
-			"MagicStaff": true,
-			"Redberry": true
 		},
-		"dropGameMoney": {
-			"min": 50,
-			"max": 60
-		},
-		"spawnRate": 20,
-		"rating": 2,
-		"monster": true,
-		"boss": false,
-		"properties": {
-			"lv1": {
+		"Luga": {
+			"elementType": "water",
+			"dropItems": {
+				"MagicStaff": true,
+				"Redberry": true
+			},
+			"dropGameMoney": {
+				"min": 50,
+				"max": 60
+			},
+			"spawnRate": 20,
+			"rating": 2,
+			"monster": true,
+			"boss": false,
+			"properties": {
 				"hp": {
 					"min": 450,
 					"max": 500
@@ -150,29 +156,31 @@ var data = {
 					"min": 1.0,
 					"max": 0.5
 				},
-				"expForNext": 200,
-				"skills": {
-					"TakeBath": true
+				"byLevel": {
+					"lv1": {
+						"expForNext": 200,
+						"skills": {
+							"TakeBath": true
+						}
+					}
 				}
 			}
-		}
-	},
-	"E2": {
-		"elementType": "light",
-		"dropItems": {
-			"MagicStaff": true,
-			"Redberry": true
 		},
-		"dropGameMoney": {
-			"min": 50,
-			"max": 60
-		},
-		"spawnRate": 20,
-		"rating": 2,
-		"monster": true,
-		"boss": false,
-		"properties": {
-			"lv1": {
+		"E2": {
+			"elementType": "light",
+			"dropItems": {
+				"MagicStaff": true,
+				"Redberry": true
+			},
+			"dropGameMoney": {
+				"min": 50,
+				"max": 60
+			},
+			"spawnRate": 20,
+			"rating": 2,
+			"monster": true,
+			"boss": false,
+			"properties": {
 				"hp": {
 					"min": 450,
 					"max": 500
@@ -189,29 +197,31 @@ var data = {
 					"min": 1.5,
 					"max": 1.0
 				},
-				"expForNext": 200,
-				"skills": {
-					"ISeeYou": true
+				"byLevel": {
+					"lv1": {
+						"expForNext": 200,
+						"skills": {
+							"ISeeYou": true
+						}
+					}
 				}
 			}
-		}
-	},
-	"E3": {
-		"elementType": "water",
-		"dropItems": {
-			"Blade": true,
-			"Redberry": true
 		},
-		"dropGameMoney": {
-			"min": 50,
-			"max": 60
-		},
-		"spawnRate": 20,
-		"rating": 2,
-		"monster": true,
-		"boss": false,
-		"properties": {
-			"lv1": {
+		"E3": {
+			"elementType": "water",
+			"dropItems": {
+				"Blade": true,
+				"Redberry": true
+			},
+			"dropGameMoney": {
+				"min": 50,
+				"max": 60
+			},
+			"spawnRate": 20,
+			"rating": 2,
+			"monster": true,
+			"boss": false,
+			"properties": {
 				"hp": {
 					"min": 550,
 					"max": 600
@@ -228,29 +238,31 @@ var data = {
 					"min": 1.0,
 					"max": 0.7
 				},
-				"expForNext": 200,
-				"skills": {
-					"DontMess": true
+				"byLevel": {
+					"lv1": {
+						"expForNext": 200,
+						"skills": {
+							"DontMess": true
+						}
+					}
 				}
 			}
-		}
-	},
-	"Woolve": {
-		"elementType": "tree",
-		"dropItems": {
-			"SilverCoin": true,
-			"Redberry": true
 		},
-		"dropGameMoney": {
-			"min": 50,
-			"max": 60
-		},
-		"spawnRate": 20,
-		"rating": 2,
-		"monster": true,
-		"boss": false,
-		"properties": {
-			"lv1": {
+		"Woolve": {
+			"elementType": "tree",
+			"dropItems": {
+				"SilverCoin": true,
+				"Redberry": true
+			},
+			"dropGameMoney": {
+				"min": 50,
+				"max": 60
+			},
+			"spawnRate": 20,
+			"rating": 2,
+			"monster": true,
+			"boss": false,
+			"properties": {
 				"hp": {
 					"min": 450,
 					"max": 500
@@ -267,29 +279,31 @@ var data = {
 					"min": 1.0,
 					"max": 0.5
 				},
-				"expForNext": 200,
-				"skills": {
-					"IHaveTear": true
+				"byLevel": {
+					"lv1": {
+						"expForNext": 200,
+						"skills": {
+							"IHaveTear": true
+						}
+					}
 				}
 			}
-		}
-	},
-	"Doki": {
-		"elementType": "light",
-		"dropItems": {
-			"GoldCoin": true,
-			"GreenBow": true
 		},
-		"dropGameMoney": {
-			"min": 500,
-			"max": 600
-		},
-		"spawnRate": 5,
-		"rating": 5,
-		"monster": false,
-		"boss": true,
-		"properties": {
-			"lv1": {
+		"Doki": {
+			"elementType": "light",
+			"dropItems": {
+				"GoldCoin": true,
+				"GreenBow": true
+			},
+			"dropGameMoney": {
+				"min": 500,
+				"max": 600
+			},
+			"spawnRate": 5,
+			"rating": 5,
+			"monster": false,
+			"boss": true,
+			"properties": {
 				"hp": {
 					"min": 900,
 					"max": 1000
@@ -306,29 +320,31 @@ var data = {
 					"min": 1.0,
 					"max": 0.5
 				},
-				"expForNext": 1000,
-				"skills": {
-					"ShiningAction": true
+				"byLevel": {
+					"lv1": {
+						"expForNext": 1000,
+						"skills": {
+							"ShiningAction": true
+						}
+					}
 				}
 			}
-		}
-	},
-	"Gangwoo": {
-		"elementType": "tree",
-		"dropItems": {
-			"GoldCoin": true,
-			"Blade": true
 		},
-		"dropGameMoney": {
-			"min": 500,
-			"max": 600
-		},
-		"spawnRate": 5,
-		"rating": 5,
-		"monster": false,
-		"boss": true,
-		"properties": {
-			"lv1": {
+		"Gangwoo": {
+			"elementType": "tree",
+			"dropItems": {
+				"GoldCoin": true,
+				"Blade": true
+			},
+			"dropGameMoney": {
+				"min": 500,
+				"max": 600
+			},
+			"spawnRate": 5,
+			"rating": 5,
+			"monster": false,
+			"boss": true,
+			"properties": {
 				"hp": {
 					"min": 1400,
 					"max": 1500
@@ -345,29 +361,31 @@ var data = {
 					"min": 1.0,
 					"max": 0.5
 				},
-				"expForNext": 1000,
-				"skills": {
-					"Slow": true
+				"byLevel": {
+					"lv1": {
+						"expForNext": 1000,
+						"skills": {
+							"Slow": true
+						}
+					}
 				}
 			}
-		}
-	},
-	"Hwasa": {
-		"elementType": "dark",
-		"dropItems": {
-			"GoldCoin": true,
-			"Sword": true
 		},
-		"dropGameMoney": {
-			"min": 500,
-			"max": 600
-		},
-		"spawnRate": 5,
-		"rating": 5,
-		"monster": false,
-		"boss": true,
-		"properties": {
-			"lv1": {
+		"Hwasa": {
+			"elementType": "dark",
+			"dropItems": {
+				"GoldCoin": true,
+				"Sword": true
+			},
+			"dropGameMoney": {
+				"min": 500,
+				"max": 600
+			},
+			"spawnRate": 5,
+			"rating": 5,
+			"monster": false,
+			"boss": true,
+			"properties": {
 				"hp": {
 					"min": 900,
 					"max": 1000
@@ -384,29 +402,31 @@ var data = {
 					"min": 1.5,
 					"max": 1.0
 				},
-				"expForNext": 1000,
-				"skills": {
-					"Breakfast": true
+				"byLevel": {
+					"lv1": {
+						"expForNext": 1000,
+						"skills": {
+							"Breakfast": true
+						}
+					}
 				}
 			}
-		}
-	},
-	"Spoon": {
-		"elementType": "dark",
-		"dropItems": {
-			"MagicStaff": true,
-			"Redberry": true
 		},
-		"dropGameMoney": {
-			"min": 50,
-			"max": 60
-		},
-		"spawnRate": 20,
-		"rating": 2,
-		"monster": true,
-		"boss": false,
-		"properties": {
-			"lv1": {
+		"Spoon": {
+			"elementType": "dark",
+			"dropItems": {
+				"MagicStaff": true,
+				"Redberry": true
+			},
+			"dropGameMoney": {
+				"min": 50,
+				"max": 60
+			},
+			"spawnRate": 20,
+			"rating": 2,
+			"monster": true,
+			"boss": false,
+			"properties": {
 				"hp": {
 					"min": 1400,
 					"max": 1500
@@ -423,29 +443,31 @@ var data = {
 					"min": 0.5,
 					"max": 1.0
 				},
-				"expForNext": 200,
-				"skills": {
-					"Hungry": true
+				"byLevel": {
+					"lv1": {
+						"expForNext": 200,
+						"skills": {
+							"Hungry": true
+						}
+					}
 				}
 			}
-		}
-	},
-	"Sopleco": {
-		"elementType": "tree",
-		"dropItems": {
-			"GreenBow": true,
-			"Redberry": true
 		},
-		"dropGameMoney": {
-			"min": 50,
-			"max": 60
-		},
-		"spawnRate": 20,
-		"rating": 2,
-		"monster": true,
-		"boss": false,
-		"properties": {
-			"lv1": {
+		"Sopleco": {
+			"elementType": "tree",
+			"dropItems": {
+				"GreenBow": true,
+				"Redberry": true
+			},
+			"dropGameMoney": {
+				"min": 50,
+				"max": 60
+			},
+			"spawnRate": 20,
+			"rating": 2,
+			"monster": true,
+			"boss": false,
+			"properties": {
 				"hp": {
 					"min": 450,
 					"max": 500
@@ -462,29 +484,31 @@ var data = {
 					"min": 1.0,
 					"max": 0.5
 				},
-				"expForNext": 200,
-				"skills": {
-					"FinalAttack": true
+				"byLevel": {
+					"lv1": {
+						"expForNext": 200,
+						"skills": {
+							"FinalAttack": true
+						}
+					}
 				}
 			}
-		}
-	},
-	"Konga": {
-		"elementType": "tree",
-		"dropItems": {
-			"SilverCoin": true,
-			"Redberry": true
 		},
-		"dropGameMoney": {
-			"min": 50,
-			"max": 60
-		},
-		"spawnRate": 20,
-		"rating": 2,
-		"monster": true,
-		"boss": false,
-		"properties": {
-			"lv1": {
+		"Konga": {
+			"elementType": "tree",
+			"dropItems": {
+				"SilverCoin": true,
+				"Redberry": true
+			},
+			"dropGameMoney": {
+				"min": 50,
+				"max": 60
+			},
+			"spawnRate": 20,
+			"rating": 2,
+			"monster": true,
+			"boss": false,
+			"properties": {
 				"hp": {
 					"min": 450,
 					"max": 500
@@ -501,13 +525,69 @@ var data = {
 					"min": 1.0,
 					"max": 0.5
 				},
-				"expForNext": 200,
-				"skills": {
-					"UsingFoce": true
+				"byLevel": {
+					"lv1": {
+						"expForNext": 200,
+						"skills": {
+							"UsingFoce": true
+						}
+					}
 				}
 			}
 		}
 	},
+	"my": {
+		"properties": {
+			"hp": {
+				"min": 30,
+				"max": 50
+			},
+			"defence": {
+				"min": 15,
+				"max": 20
+			},
+			"attackPower": {
+				"min": 45,
+				"max": 50
+			},
+			"agility": {
+				"min": 1.0,
+				"max": 0.5
+			},
+			"byLevel": {
+				"lv1": {
+					"expForNext": 200
+				},
+				"lv2": {
+					"expForNext": 300
+				},
+				"lv3": {
+					"expForNext": 400
+				},
+				"lv4": {
+					"expForNext": 500
+				},
+				"lv5": {
+					"expForNext": 600
+				},
+				"lv6": {
+					"expForNext": 700
+				},
+				"lv7": {
+					"expForNext": 800
+				},
+				"lv8": {
+					"expForNext": 900
+				},
+				"lv9": {
+					"expForNext": 1000
+				},
+				"lv10": {
+					"expForNext": 2000
+				}
+			}
+		}
+	}
 };
 
 
