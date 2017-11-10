4db13f777bf3cd811926191e2b7e21119d701243
KIMSEONHO <sinho0689@gmail.com>
2017-11-11 04:26:49 +0900

    - userProperty.js 수정(DB 구조 및 샘플데이터 자동생성코드 작성
    Signed-off-by: KIMSEONHO <sinho0689@gmail.com>

--- test/data/item.js
+++ test/data/item.js
@@ -1,278 +1,289 @@
 "use strict";
 
 var data = {
-	"Redberry": {
-		"type": {
-			"main": "material",
-			"sub": "healingPotion"
-		},
-		"slotSize": {
-			"row": 1,
-			"col": 1
-		},
-		"price": {
-			"gameMoney": 50,
-			"cash": 0
-		},
-		"property": {
-			"heal": {
-				"min": 50,
-				"max": 50
+	"weapon": {
+		"BasicSword": {
+			"type": {
+				"main": "weapon",
+				"sub": "sword"
 			},
-			"attackPower": {
-				"min": 0,
-				"max": 0
+			"slotSize": {
+				"row": 1,
+				"col": 1
 			},
-			"defence": {
-				"min": 0,
-				"max": 0
+			"price": {
+				"gameMoney": 100,
+				"cash": 0
 			},
-			"agility": {
-				"min": 0,
-				"max": 0
+			"property": {
+				"hp": {
+					"min": 0,
+					"max": 0
+				},
+				"attackPower": {
+					"min": 10,
+					"max": 30
+				},
+				"defence": {
+					"min": 0,
+					"max": 0
+				},
+				"agility": {
+					"min": 0,
+					"max": 0
+				},
 			},
+			"dropRate": 30.0,
+			"rating": 2
 		},
-		"dropRate": 30.0,
-		"rating": 1
+		"Blade": {
+			"type": {
+				"main": "weapon",
+				"sub": "sword"
+			},
+			"slotSize": {
+				"row": 1,
+				"col": 1
+			},
+			"price": {
+				"gameMoney": 100,
+				"cash": 0
+			},
+			"property": {
+				"hp": {
+					"min": 50,
+					"max": 50
+				},
+				"attackPower": {
+					"min": 100,
+					"max": 150
+				},
+				"defence": {
+					"min": 0,
+					"max": 0
+				},
+				"agility": {
+					"min": 0,
+					"max": 0
+				},
+			},
+			"dropRate": 30.0,
+			"rating": 2
+		},
+		"MagicStaff": {
+			"type": {
+				"main": "weapon",
+				"sub": "staff"
+			},
+			"slotSize": {
+				"row": 1,
+				"col": 1
+			},
+			"price": {
+				"gameMoney": 200,
+				"cash": 0
+			},
+			"property": {
+				"hp": {
+					"min": 0,
+					"max": 0
+				},
+				"attackPower": {
+					"min": 150,
+					"max": 200
+				},
+				"defence": {
+					"min": 0,
+					"max": 0
+				},
+				"agility": {
+					"min": 0,
+					"max": 0
+				},
+			},
+			"dropRate": 20.0,
+			"rating": 4
+		}
 	},
-	"BronzeRing": {
-		"type": {
-			"main": "accessory",
-			"sub": "ring"
-		},
-		"slotSize": {
-			"row": 1,
-			"col": 1
-		},
-		"price": {
-			"gameMoney": 100,
-			"cash": 0
-		},
-		"property": {
-			"heal": {
-				"min": 0,
-				"max": 0
-			},
-			"attackPower": {
-				"min": 0,
-				"max": 0
-			},
-			"defence": {
-				"min": 0,
-				"max": 0
-			},
-			"agility": {
-				"min": 1,
-				"max": 3
-			},
-		},
-		"dropRate": 30.0,
-		"rating": 1
+	"armor": {
+
 	},
-	"BasicSword": {
-		"type": {
-			"main": "weapon",
-			"sub": "sword"
-		},
-		"slotSize": {
-			"row": 1,
-			"col": 1
-		},
-		"price": {
-			"gameMoney": 100,
-			"cash": 0
-		},
-		"property": {
-			"heal": {
-				"min": 0,
-				"max": 0
+	"accessory": {
+		"BronzeRing": {
+			"type": {
+				"main": "accessory",
+				"sub": "ring"
 			},
-			"attackPower": {
-				"min": 10,
-				"max": 30
+			"slotSize": {
+				"row": 1,
+				"col": 1
 			},
-			"defence": {
-				"min": 0,
-				"max": 0
+			"price": {
+				"gameMoney": 100,
+				"cash": 0
 			},
-			"agility": {
-				"min": 0,
-				"max": 0
+			"property": {
+				"hp": {
+					"min": 0,
+					"max": 0
+				},
+				"attackPower": {
+					"min": 0,
+					"max": 0
+				},
+				"defence": {
+					"min": 0,
+					"max": 0
+				},
+				"agility": {
+					"min": 1,
+					"max": 3
+				},
 			},
-		},
-		"dropRate": 30.0,
-		"rating": 2
+			"dropRate": 30.0,
+			"rating": 1
+		}
 	},
-	"Blade": {
-		"type": {
-			"main": "weapon",
-			"sub": "sword"
-		},
-		"slotSize": {
-			"row": 1,
-			"col": 1
-		},
-		"price": {
-			"gameMoney": 100,
-			"cash": 0
-		},
-		"property": {
-			"heal": {
-				"min": 50,
-				"max": 50
+	"groceries": {
+		"Redberry": {
+			"type": {
+				"main": "material",
+				"sub": "healingPotion"
 			},
-			"attackPower": {
-				"min": 100,
-				"max": 150
+			"slotSize": {
+				"row": 1,
+				"col": 1
 			},
-			"defence": {
-				"min": 0,
-				"max": 0
+			"price": {
+				"gameMoney": 50,
+				"cash": 0
 			},
-			"agility": {
-				"min": 0,
-				"max": 0
+			"property": {
+				"hp": {
+					"min": 50,
+					"max": 50
+				},
+				"attackPower": {
+					"min": 0,
+					"max": 0
+				},
+				"defence": {
+					"min": 0,
+					"max": 0
+				},
+				"agility": {
+					"min": 0,
+					"max": 0
+				},
 			},
-		},
-		"dropRate": 30.0,
-		"rating": 2
+			"dropRate": 30.0,
+			"rating": 1
+		}
 	},
-	"MagicStaff": {
-		"type": {
-			"main": "weapon",
-			"sub": "staff"
-		},
-		"slotSize": {
-			"row": 1,
-			"col": 1
-		},
-		"price": {
-			"gameMoney": 200,
-			"cash": 0
-		},
-		"property": {
-			"heal": {
-				"min": 0,
-				"max": 0
+	"coin": {
+		"SilverCoin": {
+			"type": {
+				"main": "material",
+				"sub": "etc"
 			},
-			"attackPower": {
-				"min": 150,
-				"max": 200
+			"slotSize": {
+				"row": 1,
+				"col": 1
 			},
-			"defence": {
-				"min": 0,
-				"max": 0
+			"price": {
+				"gameMoney": 500,
+				"cash": 0
 			},
-			"agility": {
-				"min": 0,
-				"max": 0
+			"property": {
+				"hp": {
+					"min": 0,
+					"max": 0
+				},
+				"attackPower": {
+					"min": 0,
+					"max": 0
+				},
+				"defence": {
+					"min": 0,
+					"max": 0
+				},
+				"agility": {
+					"min": 0,
+					"max": 0
+				},
 			},
+			"dropRate": 20.0,
+			"rating": 4
 		},
-		"dropRate": 20.0,
-		"rating": 4
-	},
-	"SilverCoin": {
-		"type": {
-			"main": "material",
-			"sub": "etc"
-		},
-		"slotSize": {
-			"row": 1,
-			"col": 1
-		},
-		"price": {
-			"gameMoney": 500,
-			"cash": 0
-		},
-		"property": {
-			"heal": {
-				"min": 0,
-				"max": 0
+		"GoldCoin": {
+			"type": {
+				"main": "material",
+				"sub": "etc"
 			},
-			"attackPower": {
-				"min": 0,
-				"max": 0
+			"slotSize": {
+				"row": 1,
+				"col": 1
 			},
-			"defence": {
-				"min": 0,
-				"max": 0
+			"price": {
+				"gameMoney": 2000,
+				"cash": 0
 			},
-			"agility": {
-				"min": 0,
-				"max": 0
+			"property": {
+				"hp": {
+					"min": 0,
+					"max": 0
+				},
+				"attackPower": {
+					"min": 0,
+					"max": 0
+				},
+				"defence": {
+					"min": 0,
+					"max": 0
+				},
+				"agility": {
+					"min": 0,
+					"max": 0
+				},
 			},
+			"dropRate": 5.0,
+			"rating": 5
 		},
-		"dropRate": 20.0,
-		"rating": 4
-	},
-	"GoldCoin": {
-		"type": {
-			"main": "material",
-			"sub": "etc"
-		},
-		"slotSize": {
-			"row": 1,
-			"col": 1
-		},
-		"price": {
-			"gameMoney": 2000,
-			"cash": 0
-		},
-		"property": {
-			"heal": {
-				"min": 0,
-				"max": 0
+		"BronzeCoin": {
+			"type": {
+				"main": "material",
+				"sub": "etc"
 			},
-			"attackPower": {
-				"min": 0,
-				"max": 0
+			"slotSize": {
+				"row": 1,
+				"col": 1
 			},
-			"defence": {
-				"min": 0,
-				"max": 0
+			"price": {
+				"gameMoney": 300,
+				"cash": 0
 			},
-			"agility": {
-				"min": 0,
-				"max": 0
+			"property": {
+				"hp": {
+					"min": 0,
+					"max": 0
+				},
+				"attackPower": {
+					"min": 0,
+					"max": 0
+				},
+				"defence": {
+					"min": 0,
+					"max": 0
+				},
+				"agility": {
+					"min": 0,
+					"max": 0
+				},
 			},
-		},
-		"dropRate": 5.0,
-		"rating": 5
-	},
-	"BronzeCoin": {
-		"type": {
-			"main": "material",
-			"sub": "etc"
-		},
-		"slotSize": {
-			"row": 1,
-			"col": 1
-		},
-		"price": {
-			"gameMoney": 300,
-			"cash": 0
-		},
-		"property": {
-			"heal": {
-				"min": 0,
-				"max": 0
-			},
-			"attackPower": {
-				"min": 0,
-				"max": 0
-			},
-			"defence": {
-				"min": 0,
-				"max": 0
-			},
-			"agility": {
-				"min": 0,
-				"max": 0
-			},
-		},
-		"dropRate": 30.0,
-		"rating": 3
-	}	
+			"dropRate": 30.0,
+			"rating": 3
+		}
+	}
 };
 
 
--- test/data/userProperty.js
+++ test/data/userProperty.js
@@ -1,15 +1,74 @@
 "use strict";
 
+let _ = require("lodash");
+let userData = require("./user");
+let itemData = require("./item");
+var crypto = require("crypto");
+
+var weaponKey = Object.keys(itemData.weapon);
+var armorKey = Object.keys(itemData.armor);
+var accessoryKey = Object.keys(itemData.accessory);
+var groceriesKey = Object.keys(itemData.groceries);
+
+let tengableItemData = {
+	"weapon": itemData.weapon,
+	"armor": itemData.armor,
+	"accessory": itemData.accessory,
+	"groceries": itemData.groceries
+}
+
+let wearableItemData = {
+	"weapon": itemData.weapon,
+	"armor": itemData.armor,
+	"accessory": itemData.accessory
+}
+
 // user 수만큼 있음
 var data = {
 	"smadupset@naver": {
-		"level": -71,
-		"exp": -4,
-		"film": -17,
-		"gold": -75,
-		"cash": -63,
+		"level": 2,
+		"exp": 265,
+		"film": 27,
+		"gold": 934,
+		"cash": 200,
 		"inventory": {
-			"uid": {
+			"c4ca4238a0b923820dcc509a6f75849b": {
+				"name": "Lorem",
+				"property": {
+					"hp": -46,
+					"defence": -34,
+					"attackPower": 94,
+					"agility": -32
+				}
+			},
+			"c81e728d9d4c2f636f067f89cc14862c": {
+				"name": "Lorem",
+				"property": {
+					"hp": -46,
+					"defence": -34,
+					"attackPower": 94,
+					"agility": -32
+				}
+			},
+			"eccbc87e4b5ce2fe28308fd9f2a7baf3": {
+				"name": "Lorem",
+				"property": {
+					"hp": -46,
+					"defence": -34,
+					"attackPower": 94,
+					"agility": -32
+				}
+			},
+			"a87ff679a2f3e71d9181a67b7542122c": {
+				"name": "Lorem",
+				"property": {
+					"hp": -46,
+					"defence": -34,
+					"attackPower": 94,
+					"agility": -32
+				}
+			},
+			"c4ca4238a0b923820dcc5": {
 				"name": "Lorem",
 				"property": {
 					"hp": -46,
@@ -154,5 +213,108 @@ var data = {
 	}
 };
 
+_.forEach(userData, function(userValue, userKey) {
+	data[userKey] = {
+		"level": _.random(0, 10),
+		"exp": _.random(0, 100),
+		"film": _.random(0, 100),
+		"gold": _.random(0, 1000),
+		"cash": _.random(0, 500)
+	};
+
+	data[userKey].inventory = {};
+	data[userKey].equipment = {
+		"weapon": {
+		},
+		"armor": {
+		},
+		"accessory": {
+		}
+	};
+	data[userKey].bank = {};
+
+	// inventory
+	_.forEach(tengableItemData, function(itemValue, itemKey) {
+		let uid = crypto.randomBytes(10).toString('hex');
+		let itemObj, itemObjKey;
+
+		switch (itemKey) {
+			case "weapon":
+				itemObj = tengableItemData.weapon;
+				itemObjKey = weaponKey;
+				break;
+			case "armor":
+				itemObj = tengableItemData.armor;
+				itemObjKey = armorKey;
+				break;
+			case "accessory":
+				itemObj = tengableItemData.accessory;
+				itemObjKey = accessoryKey;
+				break;
+			case "groceries":
+				itemObj = tengableItemData.groceries;
+				itemObjKey = groceriesKey;
+				break;
+		}
+
+		let idx = _.random(0, itemObjKey.length - 1);
+		let detailValue = itemObj[itemObjKey[idx]];
+
+		if (detailValue) {
+			data[userKey].inventory[uid] = {};
+			data[userKey].inventory[uid].name = itemObjKey[idx];
+			data[userKey].inventory[uid].property = {
+				hp: detailValue.property.hp.min,
+				attackPower: detailValue.property.attackPower.min,
+				defence: detailValue.property.defence.min,
+				agility: detailValue.property.agility.min
+			};
+		}
+	});
+
+	// equipment
+	_.forEach(data[userKey].equipment, function(equipValue, equipKey) {
+		let uid = crypto.randomBytes(10).toString('hex');
+		let itemObj, itemObjKey, partObj;
+
+		switch (equipKey) {
+			case "weapon":
+				data[userKey].equipment.weapon = {};
+
+				itemObj = wearableItemData.weapon;
+				itemObjKey = weaponKey;
+				break;
+			case "armor":
+				data[userKey].equipment.armor = {};
+
+				itemObj = wearableItemData.armor;
+				itemObjKey = armorKey;
+				break;
+			case "accessory":
+				data[userKey].equipment.accessory = {};
+
+				itemObj = wearableItemData.accessory;
+				itemObjKey = accessoryKey;
+				break;
+		}
+
+		let idx = _.random(0, itemObjKey.length - 1);
+		let detailValue = itemObj[itemObjKey[idx]];
+
+		if (detailValue) {
+			data[userKey].equipment[equipKey][uid] = {};
+			partObj = data[userKey].equipment[equipKey][uid];
+
+			partObj.name = itemObjKey[idx];
+			partObj.property = {
+				hp: detailValue.property.hp.min,
+				attackPower: detailValue.property.attackPower.min,
+				defence: detailValue.property.defence.min,
+				agility: detailValue.property.agility.min
+			};
+		}
+	});
+
+});
 
 module.exports = data;
