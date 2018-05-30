0c7b4258d9ca213f220b79ef75b090793b28f1ed
KIMSEONHO <sinho0689@gmail.com>
2018-05-30 18:23:28 +0900

    - DB 추가(character.face, build/ 참고)
    Signed-off-by: KIMSEONHO <sinho0689@gmail.com>

--- test/data/character.js
+++ test/data/character.js
@@ -1,6 +1,41 @@
 "use strict";
 
 var data = {
+	"face": {
+		"face06": {
+			"minRequiredLevel": 1
+		},
+		"face09": {
+			"minRequiredLevel": 5
+		},
+		"face10": {
+			"minRequiredLevel": 15
+		},
+		"face14": {
+			"minRequiredLevel": 1
+		},
+		"face15": {
+			"minRequiredLevel": 7
+		},
+		"face17": {
+			"minRequiredLevel": 19
+		},
+		"face18": {
+			"minRequiredLevel": 1
+		},
+		"face20": {
+			"minRequiredLevel": 9
+		},
+		"face25": {
+			"minRequiredLevel": 25
+		},
+		"face26": {
+			"minRequiredLevel": 1
+		},
+		"face28": {
+			"minRequiredLevel": 11
+		}
+	},
 	"model": {
 		"Square": {
 			"minRequiredLevel": 1
